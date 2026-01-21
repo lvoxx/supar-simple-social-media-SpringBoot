ba5cef0dc24316d6e7afb7e19485ba24a3a4ae3c
Lvoxx <95278222+lvoxx@users.noreply.github.com>
2026-01-21 13:15:57 +0000

    Init microservice

---
+++ warehouse/src/test/java/io/github/lvoxx/warehouse/TestWarehouseApplication.java
@@ -0,0 +1,11 @@
+package io.github.lvoxx.warehouse;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(WarehouseApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
