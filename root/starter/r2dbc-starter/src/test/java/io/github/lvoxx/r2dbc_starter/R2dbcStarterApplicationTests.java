5afd8d463796afa38e7ea6d6efe919acded93a5d
Lvoxx <95278222+lvoxx@users.noreply.github.com>
2026-01-27 06:34:44 +0000

    Init r2dbc starter

---
+++ root/starter/r2dbc-starter/src/test/java/io/github/lvoxx/r2dbc_starter/R2dbcStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lvoxx.r2dbc_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R2dbcStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
