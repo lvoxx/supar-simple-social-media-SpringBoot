c2bbbb9c9571370f53d21e08e32d187012c5a5d8
Lvoxx <95278222+lvoxx@users.noreply.github.com>
2026-01-28 13:57:05 +0000

    feat(r2dbc-cassandra-starter): add R2DBC Cassandra starter module with initial configuration, application properties, and test cases

---
+++ root/starter/r2dbc-cassandra-starter/src/main/java/io/github/lvoxx/r2dbc_cassandra_starter/R2dbcCassandraStarterApplication.java
@@ -0,0 +1,13 @@
+package io.github.lvoxx.r2dbc_cassandra_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2dbcCassandraStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R2dbcCassandraStarterApplication.class, args);
+	}
+
+}
