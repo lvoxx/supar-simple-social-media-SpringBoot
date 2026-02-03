d71c3424ed6cc5fcb1eb3fdae117a2fe531c6b13
lvoxx <lvoxxartist@gmail.com>
2026-02-03 19:06:38 +0700

    Refactor BlockedUser and MutedUser models to remove inheritance from BaseEntity and add createdAt field

--- spring-services/user-service/src/main/java/io/github/lvoxx/user_service/model/MutedUser.java
+++ spring-services/user-service/src/main/java/io/github/lvoxx/user_service/model/MutedUser.java
@@ -1,9 +1,11 @@
 package io.github.lvoxx.user_service.model;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
-import io.github.lvoxx.common_core.model.BaseEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -14,13 +16,19 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
-@EqualsAndHashCode(callSuper = true)
+@EqualsAndHashCode
 @Table("muted_users")
-class MutedUser extends BaseEntity {
+class MutedUser {
+
+    @Id
+    private Long id;
 
     @Column("muter_user_id")
     private Long muterUserId;
 
     @Column("muted_user_id")
     private Long mutedUserId;
+
+    @Column("created_at")
+    private LocalDateTime createdAt;
 }
