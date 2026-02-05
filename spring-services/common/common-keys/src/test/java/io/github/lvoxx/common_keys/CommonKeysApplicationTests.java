1c1301bbee964e5fab7786d0569e7634b2761a72
lvoxx <lvoxxartist@gmail.com>
2026-02-05 21:22:06 +0700

    Init common-keys module

---
+++ spring-services/common/common-keys/src/test/java/io/github/lvoxx/common_keys/CommonKeysApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lvoxx.common_keys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonKeysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
