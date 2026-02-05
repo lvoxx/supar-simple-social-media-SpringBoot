a1990d3cd21a0328a8444151bb9a0a1cdee56c5e
lvoxx <lvoxxartist@gmail.com>
2026-02-05 20:57:07 +0700

    Add Cloudinary starter module with initial configuration and files

---
+++ spring-services/starter/cloudinary-starter/src/main/java/io/github/lvoxx/cloudinary_starter/CloudinaryStarterApplication.java
@@ -0,0 +1,13 @@
+package io.github.lvoxx.cloudinary_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudinaryStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudinaryStarterApplication.class, args);
+	}
+
+}
---
+++ spring-services/starter/cloudinary-starter/src/test/java/io/github/lvoxx/cloudinary_starter/CloudinaryStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lvoxx.cloudinary_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudinaryStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
