c457229cd67131d2dcfd371ae8b113f3caa7163c
lvoxx <lvoxxartist@gmail.com>
2026-02-08 21:03:42 +0700

    Added circuit breaker and add application configurations

--- spring-services/post-service/src/main/java/io/github/lvoxx/post_service/PostServiceApplication.java
+++ spring-services/post-service/src/main/java/io/github/lvoxx/post_service/PostServiceApplication.java
@@ -2,8 +2,16 @@ package io.github.lvoxx.post_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableR2dbcAuditing
+@EnableCaching
+@EnableKafka
+@EnableWebFlux
 public class PostServiceApplication {
 
 	public static void main(String[] args) {
--- spring-services/user-service/src/main/java/io/github/lvoxx/user_service/UserServiceApplication.java
+++ spring-services/user-service/src/main/java/io/github/lvoxx/user_service/UserServiceApplication.java
@@ -2,8 +2,16 @@ package io.github.lvoxx.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableR2dbcAuditing
+@EnableCaching
+@EnableKafka
+@EnableWebFlux
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
