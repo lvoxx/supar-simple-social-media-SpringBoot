a162f7081731feba45fc30b6b44a06e5a59cce9d
lvoxx <lvoxxartist@gmail.com>
2026-02-26 08:45:18 +0700

    Added EnableR2dbcRepositories

--- spring-services/media-service/src/main/java/io/github/lvoxx/media_service/MediaServiceApplication.java
+++ spring-services/media-service/src/main/java/io/github/lvoxx/media_service/MediaServiceApplication.java
@@ -2,8 +2,10 @@ package io.github.lvoxx.media_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
+@EnableR2dbcRepositories
 public class MediaServiceApplication {
 
 	public static void main(String[] args) {
