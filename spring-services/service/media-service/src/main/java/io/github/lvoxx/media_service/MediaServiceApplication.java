dab42ee9c84fcaa3522ec5fc6e72a67acde9818b
lvoxx <lvoxxartist@gmail.com>
2026-03-12 18:52:26 +0700

    Some fixes

--- spring-services/service/media-service/src/main/java/io/github/lvoxx/media_service/MediaServiceApplication.java
+++ spring-services/service/media-service/src/main/java/io/github/lvoxx/media_service/MediaServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication(scanBasePackages = "io.github.lvoxx")(scanBasePackages = "io.github.lvoxx")
+@SpringBootApplication(scanBasePackages = "io.github.lvoxx")
 @EnableConfigurationProperties
 public class MediaServiceApplication {
 
